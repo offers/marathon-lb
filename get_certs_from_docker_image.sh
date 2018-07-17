570df228aaf34f213fa0a630919ebdcb8ba707c2
Chris Kite <chris@chriskite.com>
2018-07-17 15:50:27 -0500

    update certs

--- get_certs_from_docker_image.sh
+++ get_certs_from_docker_image.sh
@@ -3,5 +3,5 @@
 IFS=$'\n'       # make newlines the only separator
 set -f          # disable globbing
 for i in $(cat < "$1"); do
-  docker cp marathon:$i certs/
+  docker cp marathon-lb:$i certs/
 done
